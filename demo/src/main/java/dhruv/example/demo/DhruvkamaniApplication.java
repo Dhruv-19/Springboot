bd7cbb6896357fec3f15a809df7561ed86db6451
Dhruv-19 <dhruvkamani2001@gmail.com>
2024-05-27 12:49:08 +0530

    first commit

---
+++ demo/src/main/java/dhruv/example/demo/DhruvkamaniApplication.java
@@ -0,0 +1,13 @@
+package dhruv.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhruvkamaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhruvkamaniApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/dhruv/example/demo/DhruvkamaniApplicationTests.java
@@ -0,0 +1,13 @@
+package dhruv.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhruvkamaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
